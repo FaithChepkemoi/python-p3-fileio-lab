e5f8bc94166cd73e5fac9c3fcf47edc1f93ed0fc
FaithChepkemoi <faithhope017@gmail.com>
2025-03-03 03:45:49 +0300

    fileio

--- lib/file_io.py
+++ lib/file_io.py
@@ -1,8 +1,24 @@
+
 def write_file(file_name, file_content):
-    pass
+    """
+    Creates or overwrites a .txt file with the given content.
+    """
+    with open(f"{file_name}.txt", "w") as file:
+        file.write(file_content)
+
 
 def append_file(file_name, append_content):
-    pass
+    """
+    Appends content to an existing .txt file.
+    """
+    with open(f"{file_name}.txt", "a") as file:
+        file.write(append_content)
+
+
 
 def read_file(file_name):
-    pass
+    """
+    Reads and returns the content of a .txt file.
+    """
+    with open(f"{file_name}.txt", "r") as file:
+        return file.read()
